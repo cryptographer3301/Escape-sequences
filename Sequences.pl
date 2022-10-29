0ca7bc856e332f3156a82f9eeed89391ec8bac47
user32dll <116982494+cryptographer3301@users.noreply.github.com>
2022-10-29 23:47:06 +0100

    Add files via upload

---
+++ Sequences.pl
@@ -0,0 +1,10 @@
+#escape sequences in Perl
+$test = "name is dll is different from \"dll\" \n"; #to make dll inside of string quotes .
+print $test;
+
+print "Do you hear the bells? \n";
+print "Do you see the ga\t\tp?\n";
+print "The odd \uone out\n";
+print "MY N\lEW GUITAR!\n";
+print "the next \Ucase\n";
+print "THE LAST \LCASE\n";
